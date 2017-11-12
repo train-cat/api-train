bf07549683ea575e951f717e8035a39dc4b4ddec
Eraac <kevin@labesse.me>
2017-11-12 23:19:02 +0100

    int to string (env var)

--- app/repositories/init.go
+++ app/repositories/init.go
@@ -33,14 +33,14 @@ func InitDatabase(_ *aah.Event) {
 	exitOnError("password", ok)
 	hostname, ok := config.String("hostname")
 	exitOnError("hostname", ok)
-	port, ok := config.Int("port")
+	port, ok := config.String("port")
 	exitOnError("port", ok)
 	name, ok := config.String("name")
 	exitOnError("name", ok)
 	debug, ok := config.Bool("debug")
 	exitOnError("debug", ok)
 
-	d, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, hostname, port, name))
+	d, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, hostname, port, name))
 
 	if err != nil {
 		log.Errorf("Can't open database connection: %s", err.Error())
